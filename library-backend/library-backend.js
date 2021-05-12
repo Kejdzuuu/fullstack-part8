cf46c36f48e2c65df4388bd10f84bdc205567930
Maciej Modrzejewski <kejdzuuu@gmail.com>
2021-05-12 13:14:38 +0200

    Ex. 8.7

--- library-backend/library-backend.js
+++ library-backend/library-backend.js
@@ -106,6 +106,10 @@ const typeDefs = gql`
       author: String!
       genres: [String!]
     ): Book
+    editAuthor(
+      name: String!
+      setBornTo: Int!
+    ): Author
   }
 `
 
@@ -137,6 +141,16 @@ const resolvers = {
         authors = authors.concat(author)
       }
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(author => author.name === args.name)
+      if (!author) {
+        return null
+      }
+
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
